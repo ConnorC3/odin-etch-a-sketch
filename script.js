d62d580b23febe59e824f9759fe276af5b154894
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-02-23 02:18:28 -0500

    Add cursor trail to grid and button to change size

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+// get elements
 const container = document.querySelector(".container");
+const gridButton = document.querySelector(".change-grid");
 
-for (let i = 0; i < 16; i++){
+let numSquares = 16;
+
+gridButton.addEventListener("click", () => {
+    input = prompt("Change grid size: ");
+    numSquares = parseInt(input)
+});
+
+
+for (let i = 0; i < numSquares; i++){
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
+
+    square.addEventListener("mouseover", () => changeColor(square), {
+        once: true
+    });
+}
+
+function changeColor(elem){
+    elem.classList.add("active");
 }
